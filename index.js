d428d15f42d8ab1cf020abf156c12d250e397142
jbfronda <frondajohnbryan@gmail.com>
2023-03-02 20:31:15 +0800

    added tooltips

--- index.js
+++ index.js
@@ -17,3 +17,6 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
